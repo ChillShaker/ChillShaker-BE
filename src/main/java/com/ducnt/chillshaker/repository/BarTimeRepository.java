ced07641705459df15e44cc53b5402d7a5ab7af6
javisngo <ngotriduc0411@gmail.com>
2024-12-21 17:00:09 +0700

    #4 - JavisNgo add BarTimeRepository file

---
+++ src/main/java/com/ducnt/chillshaker/repository/BarTimeRepository.java
@@ -0,0 +1,7 @@
+package com.ducnt.chillshaker.repository;
+
+import com.ducnt.chillshaker.model.BarTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BarTimeRepository extends JpaRepository<BarTime, Integer> {
+}
