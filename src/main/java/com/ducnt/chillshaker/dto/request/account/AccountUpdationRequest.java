88287fd0b663575c1b4d3936cade75dfeccaa8fe
javisngo <ngotriduc0411@gmail.com>
2024-12-22 14:52:51 +0700

    #8 - Update CustomExceptionHandler

--- src/main/java/com/ducnt/chillshaker/dto/request/account/AccountUpdationRequest.java
+++ src/main/java/com/ducnt/chillshaker/dto/request/account/AccountUpdationRequest.java
@@ -1,6 +1,7 @@
 package com.ducnt.chillshaker.dto.request.account;
 
 import com.ducnt.chillshaker.validator.DobConstraint;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -17,16 +18,16 @@ import java.util.Set;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
 public class AccountUpdationRequest {
-    @Size(min = 3, max = 100, message = "FullName must be at range 3 to 100 character")
+    @Size(min = 4, message = "FULLNAME_INVALID")
     String fullName;
-    @Size(min = 6, message = "Password must be at least 6 character ")
+    @Email(message = "EMAIL_INVALID")
+    String email;
+    @Size(min = 6, message = "PASSWORD_INVALID")
     String password;
-    @Pattern(regexp = "^(0|\\+84)([3|5|7|8|9])+([0-9]{8})\\b$")
+    @Pattern(regexp = "^(0|\\+84)([3|5|7|8|9])+([0-9]{8})\\b$", message = "PHONE_INVALID")
     String phone;
     @NotBlank(message = "Address must not be blank")
     String address;
-    @DobConstraint(min = 18)
-    LocalDate dob;
 
     Set<String> roles = new HashSet<>();
 }
