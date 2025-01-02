9f021cd8482115dc2ad70f601178017921817d9a
javisngo <ngotriduc0411@gmail.com>
2025-01-02 10:26:55 +0700

    #38 - Update deletion logic to set status to false

--- src/main/java/com/ducnt/chillshaker/repository/DrinkRepository.java
+++ src/main/java/com/ducnt/chillshaker/repository/DrinkRepository.java
@@ -1,6 +1,8 @@
 package com.ducnt.chillshaker.repository;
 
 import com.ducnt.chillshaker.model.Drink;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,6 +14,8 @@ import java.util.UUID;
 @Repository
 public interface DrinkRepository extends JpaRepository<Drink, UUID>, JpaSpecificationExecutor<Drink> {
     boolean existsDrinkByName(String name);
+
+    Page<Drink> findAllByStatusFalse(Pageable pageable);
 
     @EntityGraph(attributePaths = {"drinkCategory"})
     @Override
--- src/main/java/com/ducnt/chillshaker/repository/MenuRepository.java
+++ src/main/java/com/ducnt/chillshaker/repository/MenuRepository.java
@@ -1,6 +1,8 @@
 package com.ducnt.chillshaker.repository;
 
 import com.ducnt.chillshaker.model.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, UUID>, JpaSpecificationExecutor<Menu> {
+    Page<Menu> findAllByStatusFalse(Pageable pageable);
 }
